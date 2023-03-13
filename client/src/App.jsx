7a181963779155c44505259e66fd2cb82abc9bde
McCoy Didericksen <mccoy.did@gmail.com>
2023-03-14 08:20:54 +0900

    updating simpleMovingAvg strategy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
+import React from 'react';
+import axios from 'axios';
 import StockSearch from './components/StockSearch';
 import DatePicker from './components/DatePicker';
 import InvestmentPicker from './components/InvestmentPicker';
 import AlgoSelect from './components/AlgoSelect';
-import React from 'react';
-import axios from 'axios';
+import Strategies from './utils/tradingAlgos';
 
 function App() {
   const [stocks, setStocks] = React.useState(null);
@@ -12,8 +13,8 @@ function App() {
   const [investment, setInvestment] = React.useState(10000);
   const [algo, setAlgo] = React.useState(null);
   const [prices, setPrices] = React.useState(null);
+  const [results, setResults] = React.useState(null);
   const baseURL = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=';
-  console.log(prices)
   const selection = {
     symbol,
     startDate,
@@ -35,14 +36,22 @@ function App() {
                   console.log(selection);
                   axios
                     .get(baseURL + `${symbol}&outputsize=full&apikey=${import.meta.env.VITE_API_KEY}`)
-                    .then((response) => {
+                    .then(async (response) => {
+                      if(!response.data['Time Series (Daily)']) {
+                        alert(response.data.note)
+                        return
+                      }
                       const filtered = Object.keys(response.data['Time Series (Daily)']).filter((key) => {
                         const date = new Date(key);
                         const today = new Date();
                         const diff = Math.floor((today - date) / (1000 * 60 * 60 * 24));
                         return diff <= startDate * 30;
+
                       });
-                      setPrices(filtered.map((key) => [parseInt(response.data['Time Series (Daily)'][key]['4. close']), key]));
+                      const filteredPrices = filtered.map((key) => [parseFloat(response.data['Time Series (Daily)'][key]['4. close']), key]);
+                      const results = await Strategies[algo](filteredPrices, investment);
+                      await setPrices(filteredPrices);
+                      await setResults(results);
                     });
                 }}
               >Submit</button>
--- client/src/utils/tradingAlgos.js
+++ client/src/utils/tradingAlgos.js
@@ -1,34 +1,90 @@
-export default Strategies = {
-    simpleMovingAvg: (cash, prices) => {
-        // Set the length of the moving average window and shares to 0
-            const maLength = 3;
-            let shares = 0;
-            // An array to hold the moving averages
-            const movingAverages = [];
+const Strategies = {
+  simpleMovingAvg: (prices, cash) => {
+    const initialCash = cash;
+    // Set the length of the moving average window and shares to 0
+    const maLength = 5;
+    let shares = 0;
+    // An array to hold the moving averages
+    const movingAverages = [];
 
-            // Calculate the moving averages
-            for (let i = maLength; i <= prices.length; i++) {
-            const sum = prices.slice(i - maLength, i).reduce((acc, val) => acc + val);
-            const ma = sum / maLength;
-            movingAverages.push(ma);
-            }
-
-            // Buy or sell based on the moving average crossover
-            for (let i = maLength; i < prices.length; i++) {
-            const prevMA = movingAverages[i - maLength];
-            const currMA = movingAverages[i - maLength + 1];
-            const price = prices[i];
-            if (prevMA < price && currMA >= price) {
-                // Sell all shares
-                cash += shares * price;
-                shares = 0;
-            } else if (prevMA >= price && currMA < price) {
-                // Buy as many shares as possible
-                shares += Math.floor(cash / price);
-                cash -= shares * price;
-            }
-            }
-            return [cash.toFixed(2), shares];
-
+    // Calculate the moving averages
+    for (let i = maLength; i <= prices.length; i++) {
+      const sum = prices.slice(i - maLength, i).reduce((acc, val) => {
+        return acc + val[0];
+      }, 0);
+      const ma = Math.round((sum / maLength) * 100) / 100;
+      movingAverages.push(ma);
     }
-}
+
+    const tradingHistory = {
+      cash: cash,
+      buyCount: 0,
+      sellCount: 0,
+      history: [
+        {
+          cash: cash,
+          shares: shares,
+          price: prices[0][0],
+          date: prices[0][1],
+          action: 'none',
+        },
+      ],
+    };
+
+    // Buy or sell based on the moving average crossover
+    for (let i = maLength; i < prices.length; i++) {
+      const prevMA = movingAverages[i - maLength];
+      const currMA = movingAverages[i - maLength + 1];
+      const price = prices[i][0];
+      if (prevMA < price && currMA >= price) {
+        // Sell all shares
+        cash += shares * price;
+        if (tradingHistory.buyCount > tradingHistory.sellCount) {
+          const history = {
+            cash,
+            shares,
+            price: prices[i][0],
+            date: prices[i][1],
+            action: 'sell',
+          };
+          tradingHistory.history.push(history);
+          tradingHistory.sellCount++;
+        }
+        shares = 0;
+      } else if (prevMA >= price && currMA < price) {
+        // Buy as many shares as possible
+        shares += Math.floor(cash / price);
+        cash -= shares * price;
+        if (tradingHistory.sellCount >= tradingHistory.buyCount) {
+          const history = {
+            cash,
+            shares,
+            price: prices[i][0],
+            cost: shares * price,
+            date: prices[i][1],
+            action: 'buy',
+          };
+          tradingHistory.history.push(history);
+          tradingHistory.buyCount++;
+        }
+      }
+    }
+    if (shares > 0) {
+      cash += shares * prices[prices.length - 1][0];
+      tradingHistory.history.push({
+        cash,
+        shares,
+        price: prices[prices.length - 1][0],
+        date: prices[prices.length - 1][1],
+        action: 'sell',
+      });
+      tradingHistory.sellCount++;
+    }
+
+    tradingHistory.cash = cash;
+    tradingHistory.profit = cash - initialCash;
+    return tradingHistory;
+  },
+};
+
+export default Strategies;
