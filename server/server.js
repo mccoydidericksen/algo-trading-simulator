83ae5b6258a3798979898bb1ce5ab71487956467
McCoy Didericksen <mccoy.did@gmail.com>
2023-03-20 16:41:13 -0600

    adding routes to server.js

--- server/server.js
+++ server/server.js
@@ -24,6 +24,15 @@ if (process.env.NODE_ENV === 'production') {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+app.get('/history', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
